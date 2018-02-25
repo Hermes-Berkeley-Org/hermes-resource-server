a3599174ef073eadb83301993981b46adfdd5c27
Ajay Raj <ajay@airbears2-10-142-33-25.airbears2.1918.berkeley.edu>
2018-02-24 20:18:23 -0800

    create html

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Anthony > Kian > Kartik"
+@app.route('/')
+def index():
+    return "Hermes"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('public/hello.html', name=name)
