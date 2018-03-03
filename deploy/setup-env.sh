16e1c0cf6d45f7c9f0d6c5547615891f307d6f9c
Ajay Raj <ajay@airbears2-10-142-34-167.airbears2.1918.berkeley.edu>
2018-03-02 23:23:40 -0800

    create lecture functionality

--- deploy/setup-env.sh
+++ deploy/setup-env.sh
@@ -2,3 +2,4 @@
 export FLASK_APP=app.py
 export MONGODB_URI=mongodb://heroku_kfcpzsz2:jj5ieg0th7dgdvbl3uvrgmmo9q@ds147518.mlab.com:47518/heroku_kfcpzsz2
 export DATABASE_NAME=heroku_kfcpzsz2
+export SECRET_KEY=yourmom
