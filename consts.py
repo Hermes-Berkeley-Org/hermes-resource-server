5cd98921debb1d7711534c20428b77a96c74b201
Ajay Raj <ajay@MacBook-Pro-117.local>
2018-06-17 11:52:19 -0700

    quick error fix

--- consts.py
+++ consts.py
@@ -6,4 +6,4 @@ INSTRUCTOR = "instructor"
 
 OK_ROLES = [STUDENT, LAB_ASSISTANT, GRADER, STAFF, INSTRUCTOR]
 
-NO_CAPTION_TRACK_MESSAGE = 'Please make sure that (a) you own this video, and (b) there is a caption track associated with this track: see https://support.google.com/youtube/answer/2734796?hl=en, and try again.'
+NO_CAPTION_TRACK_MESSAGE = 'Please make sure that (a) you own this video, and (b) there is a caption track associated with this video: see https://support.google.com/youtube/answer/2734796?hl=en, and try again.'
